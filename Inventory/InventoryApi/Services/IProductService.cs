a973f55c92c4a6388dde3b2ab044c74be28973fa
Elmer Lee <elmer.lee@gmail.com>
2025-11-18 12:13:15 -0600

    align update/create product DTOs (should just be the same)

--- Inventory/InventoryApi/Services/IProductService.cs
+++ Inventory/InventoryApi/Services/IProductService.cs
@@ -7,7 +7,7 @@ namespace InventoryApi.Services
         Task<IEnumerable<ProductDto>> GetAllAsync();
         Task<ProductDto?> GetByIdAsync(int id);
         Task<ProductDto> CreateAsync(CreateProductDto dto);
-        Task<bool> UpdateAsync(int id, CreateProductDto dto);
+        Task<bool> UpdateAsync(int id, UpdateProductDto dto);
         Task<bool> DeleteAsync(int id);
     }
 }
